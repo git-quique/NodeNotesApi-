58236f35186645ec36b672ceb4e28298c5ee98b3
Quique <quique@hey.com>
2021-03-04 12:40:41 +1100

    3 Add Basic Error Handling to an Express 5 App
    - Error path
    - `app.use` to catch error and respond nicely

--- index.js
+++ index.js
@@ -8,8 +8,21 @@ app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   res.json({ message: 'API is up and running 🚀!!!'});
+});
+
+app.get('/error', (req, res) => {
+  throw new Error('oops I made a mistake');
+})
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not found 😢'});
+});
+
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  res.status(500).json({ message: `${err.message} 💥` });
 })
 
 app.listen(PORT, () => {
   console.log('APP listening on port 3000 🎧!!!');
-})
+});
