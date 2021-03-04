bedfb62b6bb34dcc15c158b7c20e88bbb8a86113
Quique <quique@hey.com>
2021-03-04 12:22:43 +1100

    2 Add Basic Logging to an Express 5 App with morgan and reloading with nodedemon

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import express from 'express';
+import morgan from 'morgan';
 
 const app = express();
 const PORT = 3000;
+
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   res.json({ message: 'API is up and running 🚀!!!'});
 })
 
-app.listen(PORT)
+app.listen(PORT, () => {
+  console.log('APP listening on port 3000 🎧!!!');
+})
